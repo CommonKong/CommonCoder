ce872ed7fdc09af929d1d887893821cea048cacd
백승훈 <dayasterism@naver.com>
2012-11-02 20:44:31 +0900

    2+

---
+++ Assets/Hello.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class Hello : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		Debug.Log("hello,World!!!!!!!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
